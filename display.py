5b1ce468e8dc196bca69186fc63a5f3dcf29cc98
JohnProgramming08 <157177709+JohnProgramming08@users.noreply.github.com>
2024-06-04 21:22:47 +0000

    Updated main.py and display.py code

--- display.py
+++ display.py
@@ -1,63 +1,56 @@
 import customtkinter as ctk
 
-class Page:
-  def __init__(self, window):
-    self.root = window
-    self.page = ctk.CTkFrame(self.root)
+class Page(ctk.CTkFrame):
+  def __init__(self, parent):
+    super().__init__(parent)
     self.title_font = ctk.CTkFont(family="Roboto", size=20, weight="bold", underline=True)
     self.heading_font = ctk.CTkFont(family="arial", size=14, weight="bold")
     self.normal_font = ctk.CTkFont(family="arial", size=13)
     self.subheading_font = ctk.CTkFont(family="arial", size=13, underline=True)
-  
-  #creates all frames meeded for page
+
+  #creates all frames needed
   def create_frames(self):
     pass
 
-  #creates widgets needed for page
+  #creates all widgets needed
   def create_widgets(self):
     pass
 
-  #changes pages
+  #changes to the specified page
   def change_pages(self, current_page, next_page):
-    current_page.destroy()
-    next_page.pack()
-    
+    current_page.pack_forget()
+    next_page.pack(fill='both', expand=True)
+
 
 class LoginPage(Page):
-  def __init__(self, window, sign_up):
-    super().__init__(window)
-    self.login_page = ctk.CTkFrame(self.root)
-    self.login_page.pack()
+  def __init__(self, parent, sign_up):
+    super().__init__(parent)
     self.sign_up = sign_up
+    self.create_frames()
+    self.create_widgets()
 
-  #creates frames for the login page
   def create_frames(self):
-    self.information_frame = ctk.CTkFrame(self.login_page)
+    self.information_frame = ctk.CTkFrame(self)
     self.information_frame.grid(row=0, column=0, padx=20, pady=20)
-    self.login_frame = ctk.CTkFrame(self.login_page)
+    self.login_frame = ctk.CTkFrame(self)
     self.login_frame.grid(row=0, column=1, padx=20, pady=20)
-    self.contact_frame = ctk.CTkFrame(self.login_page)
+    self.contact_frame = ctk.CTkFrame(self)
     self.contact_frame.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
 
-  #creates widgets for the login page
   def create_widgets(self):
     self.title = ctk.CTkLabel(self.information_frame, text="Information", font=self.title_font)
     self.title.grid(row=0, column=0)
 
-    #55 characters per line
     self.purpose = ctk.CTkLabel(self.information_frame, text="This is a revision/study app to help UK students revise.\n", font=self.heading_font)
     self.purpose.grid(row=1, column=0, padx=5)
-    self.purpose2 = ctk.CTkLabel(self.information_frame, text="This app has spaced repetition algorithms, downloadable\npast papers, quizes and more! If you would wish to try\nit out then please press sign in.", font=self.normal_font)
+    self.purpose2 = ctk.CTkLabel(self.information_frame, text="This app has spaced repetition algorithms, downloadable\npast papers, quizzes and more! If you would wish to try\nit out then please press sign in.", font=self.normal_font)
     self.purpose2.grid(row=2, column=0, padx=5)
 
-    #contact information
     self.contact_title = ctk.CTkLabel(self.contact_frame, text="Contact", font=self.title_font)
     self.contact_title.grid(row=0, column=0)
-
     self.contact_purpose = ctk.CTkLabel(self.contact_frame, text="If you have any questions or concerns please contact me at: dylan08code.gmail.com", font=self.heading_font)
     self.contact_purpose.grid(row=1, column=0, padx=5)
-  
-    #prompts the user to enter account details
+
     self.login_title = ctk.CTkLabel(self.login_frame, text="Account Details", font=self.title_font)
     self.login_title.grid(row=0, column=0)
     
@@ -68,75 +61,69 @@ class LoginPage(Page):
 
     self.login_button = ctk.CTkButton(self.login_frame, text="Login", width=100)
     self.login_button.grid(row=3, column=0, padx=5, pady=5)
-    self.sign_up_button = ctk.CTkButton(self.login_frame, text="Sign Up", command = self.sign_up, width=100)
+    self.sign_up_button = ctk.CTkButton(self.login_frame, text="Sign Up", command=self.sign_up, width=100)
     self.sign_up_button.grid(row=4, column=0, padx=5, pady=5)
 
+
 class HomePage(Page):
-  def __init__(self, window, wrong, right, xp):
-      super().__init__(window)
-      self.wrong = wrong
-      self.right = right
-      self.total_answered = wrong + right
-      if self.total_answered == 0:
-        self.percentage_right = 0
-      else:
-        self.percentage_right = int((right / (self.total_answered)) * 100)
+  def __init__(self, parent, wrong, right, xp):
+    super().__init__(parent)
+    self.wrong = wrong
+    self.right = right
+    self.total_answered = wrong + right
+    if self.total_answered == 0:
+      self.percentage_right = 0
+    else:
+      self.percentage_right = int((right / self.total_answered) * 100)
       self.xp = xp
-      self.page_tabs = ctk.CTkTabview(self.root)
-      self.page_tabs.pack()
-      self.home_tab = self.page_tabs.add("Home")
-      self.home_page = ctk.CTkFrame(self.home_tab)
-      self.home_page.pack()
+    self.create_frames()
+    self.create_widgets()
 
-    
-
-  #creates frames for the home page
   def create_frames(self):
-    self.stats_frame = ctk.CTkFrame(self.home_page)
+    self.stats_frame = ctk.CTkFrame(self)
     self.stats_frame.grid(row=1, column=0, padx=20, pady=20)
-    self.schedule_frame = ctk.CTkFrame(self.home_page)
+    self.schedule_frame = ctk.CTkFrame(self)
     self.schedule_frame.grid(row=1, column=1, padx=20, pady=20)
-    
-  
-  #creates widgets for the home page
+
   def create_widgets(self):
-    #titles
     self.stats_title = ctk.CTkLabel(self.stats_frame, text="Stats", font=self.heading_font)
     self.stats_title.grid(row=0, column=0)
     self.schedule_title = ctk.CTkLabel(self.schedule_frame, text="Revision Schedule", font=self.heading_font)
     self.schedule_title.grid(row=0, column=0)
 
-    #stats
-    self.questions_answered = ctk.CTkLabel(self.stats_frame, text=f"Questions answered: {self.right + self.wrong}" , font=self.normal_font)
+    self.questions_answered = ctk.CTkLabel(self.stats_frame, text=f"Questions answered: {self.right + self.wrong}", font=self.normal_font)
     self.questions_answered.grid(row=2, column=0)
     self.percentage_display = ctk.CTkLabel(self.stats_frame, text=f"Percentage right: {self.percentage_right}%", font=self.normal_font)
     self.percentage_display.grid(row=3, column=0)
     self.xp_display = ctk.CTkLabel(self.stats_frame, text=f"XP: {self.xp}", font=self.normal_font)
     self.xp_display.grid(row=4, column=0)
+
+
+class App(ctk.CTk):
+  def __init__(self):
+    super().__init__()
+    self.title("Revision App")
+    self.login_page = LoginPage(self, "")
+    self.login_page.pack()
+
+    self.tabs = ctk.CTkTabview(self)
+    self.home_tab = self.tabs.add("Home")
+    self.home_page = HomePage(self.home_tab, wrong=5, right=4, xp=65)
+    self.home_page.pack(fill='both', expand=True)
+
+  def error_message(self, message):
+    error_window = ctk.CTkToplevel(self, fg_color="red")
+    error_window.title("Error")
+    error = ctk.CTkLabel(error_window, text=message)
+    error.pack()
     
-    
-    
-  
-    
+  #testing purposes
+  def show_login_page(self):
+    self.login_page = LoginPage(self, self.show_home_page)
+    self.login_page.pack(fill='both', expand=True)
+
+  def show_home_page(self):
+    self.login_page.pack_forget()
+    self.home_page.pack(fill='both', expand=True)
 
 
-"""
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
-
-root = ctk.CTk()
-root.title("Revision App")
-
-
-#home_page = HomePage(root, 5, 4, 65)
-#home_page.create_frames()
-#home_page.create_widgets()
-
-
-login_page = LoginPage(root)
-login_page.create_frames()
-login_page.create_widgets()
-
-root.mainloop()
-
-"""
--- main.py
+++ main.py
@@ -1,47 +1,29 @@
 import customtkinter as ctk
-from display import LoginPage, HomePage
+from display import LoginPage, HomePage, App
 from login_function import sign_up
 from database_management import get_stats
 
 def main():
-  #setting up root window
-  ctk.set_appearance_mode("dark")
-  ctk.set_default_color_theme("blue")
-  root = ctk.CTk()
-  root.title("Revision App")
-        
-  login_page1 = LoginPage(root, "")
-  login_page1.create_frames()
-  login_page1.create_widgets()
-
-  #displays an error message
-  def error_message(message):
-    error_window = ctk.CTkToplevel(root, fg_color="red")
-    error_window.title("Error")
-    error = ctk.CTkLabel(error_window, text=message)
-    error.pack()
-
+  app = App()
   #if the username is valid, creates new account 
   def sign_up2():
-    username = login_page1.username_entry.get()
-    password = login_page1.password_entry.get()
+    username = app.login_page.username_entry.get()
+    password = app.login_page.password_entry.get()
     attempt = sign_up(username, password)
-    if attempt == True:
+    if attempt is True:
       stats = get_stats(username)
-      home_page = HomePage(root, stats[1], stats[2], stats[3])
-      home_page.create_frames()
-      home_page.create_widgets()
-      login_page1.change_pages(login_page1.login_page, home_page.page_tabs)
+      app.home_page = HomePage(app.home_tab, stats[2], stats[1], stats[3])
+      app.login_page.change_pages(app.login_page, app.tabs)
 
     #returns an appropriate error message 
     elif attempt == "used":
-      error_message("Username already taken")
+      app.error_message("Username already taken")
 
     else:
-      error_message("Spaces are not allowed")
+      app.error_message("Spaces are not allowed")
       
-  login_page1.sign_up_button.configure(command=sign_up2)
-  root.mainloop()
+  app.login_page.sign_up_button.configure(command=sign_up2)
+  app.mainloop()
 
 if __name__ == "__main__":
   main()
